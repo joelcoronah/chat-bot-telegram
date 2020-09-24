9d4136cf8ab581369ee5b994a50a68695dcef48f
joelcoronah <joelcoronah@gmail.com>
2020-09-24 00:20:55 -0400

    :sparkles: feat(modules/bots): add new bot to response messages

---
+++ src/modules/bots/bots.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class BotsService implements OnModuleInit {
+
+    onModuleInit() {
+        this.botMessage();
+    }
+
+    botMessage() {        
+        process.env.NTBA_FIX_319 = "1";
+        const TelegramBot = require('node-telegram-bot-api');
+        
+        const token = 'token';
+        
+        const bot = new TelegramBot(token, { polling: true });
+    
+        bot.on('message', (msg) => {
+            let Hi = "hi";
+            if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+                bot.sendMessage(msg.from.id, "Hello " + msg.from.first_name + " what would you like to know about me ?");
+            }
+            
+            let response = "Who are you";
+            if (msg.text.toString().toLowerCase().includes("who")) {
+                bot.sendMessage(msg.chat.id, "I am an intelligent telegram robot, built with Nest.js. Thanks for asking");
+            }
+
+            let response2 = "Do you love JavaScript";
+            if (msg.text.toString().toLowerCase().includes("javascript")) {
+                bot.sendMessage(msg.from.id, "Oh, did I hear you say JavaScript? \n I really love JavaScript");
+            }
+        });
+        
+    }
+}
